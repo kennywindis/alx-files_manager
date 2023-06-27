ac7aa3c46809e5484b873cdaeb15e2fe368c9cd9
kennywindis <kennyphilip100@gmail.com>
2023-06-27 04:26:43 -0700

    fm

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import Routes from './routes/index';
+const express = require('express');
+const cRouting = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-Routes(app);
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+cRouting(app);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-export default app;
+module.exports = app;
