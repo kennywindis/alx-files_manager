42a05fb3e77b931d927c67341bcfcefc22bd5d0a
kennywindis <kennyphilip100@gmail.com>
2023-06-27 04:16:29 -0700

    fm

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,59 +1,31 @@
-/* eslint-disable */
-import { ObjectId } from 'mongodb';
+import UtilController from './UtilController';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
-const sha1 = require('sha1');
-
-// POST /users should create a new user in DB
-export async function postNew(req, res) {
-
-  try {
-    const userEmail = req.body.email;
-    if (!userEmail) {
-      return res.status(400).send({
-        error: 'Missing email',
-      });
+export default class UsersController {
+  static async postNew(request, response) {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      response.status(400).json({ error: `Missing ${!email ? 'email' : 'password'}` }).end();
+    } else if (await dbClient.userExists(email)) {
+      response.status(400).json({ error: 'Already exist' }).end();
+    } else {
+      try {
+        const passwordHash = UtilController.SHA1(password);
+        const insert = await dbClient.newUser(email, passwordHash);
+        const { _id } = insert.ops[0];
+        const _email = insert.ops[0].email;
+        response.status(201).json({ id: _id, email: _email }).end();
+      } catch (err) {
+        response.status(400).json({ error: err.message }).end();
+      }
     }
+  }
 
-    const userPassword = req.body.password;
-    if (!userPassword) {
-      return res.status(400).send({
-        error: 'Missing password',
-      });
-    }
-    
-    let existingEmail = await dbClient.db.collection('users').findOne({ email: userEmail });
-    if (existingEmail) {
-      return res.status(400).send({
-        error: 'Already exist',
-      });
-    }
-
-    let userId;
-    const hashedPw = sha1(userPassword);
-    const newUser = {
-      email: userEmail,
-      password: hashedPw,
-    };
-
-    try {
-      await dbClient.db.collection('users').insertOne(newUser, (err) => {
-        userId = newUser._id;
-        return res.status(201).send({
-          email: userEmail,
-          id: userId,
-        });
-      });
-    } catch (err) {
-      return res.status(err.status).send({
-        'error': err,
-      });
-    }
-
-  } catch (error) {
-    return res.status(500).send({
-      error: 'Server error',
-    });
+  static async getMe(request, response) {
+    const { usr } = request;
+    delete usr.password;
+    usr.id = usr._id;
+    delete usr._id;
+    response.status(200).json(usr).end();
   }
 }
