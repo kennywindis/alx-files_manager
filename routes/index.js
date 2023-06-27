019cdabdaa2c95f7128a4f2ffa6183ace030665a
kennywindis <kennyphilip100@gmail.com>
2023-06-27 04:27:45 -0700

    fm

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,64 @@
-import { Router } from 'express';
-
+import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
-import UtilController from '../controllers/UtilController';
 
-const router = Router();
+const cRouting = (app) => {
+  const router = express.Router();
+  app.use('/', router);
 
-router.use((request, response, next) => {
-  const paths = ['/connect'];
-  if (!paths.includes(request.path)) {
-    next();
-  } else if (!request.headers.authorization) {
-    response.status(401).json({ error: 'Unauthorized' }).end();
-  } else {
-    next();
-  }
-});
+  // AppController check API
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-router.use((request, response, next) => {
-  const paths = ['/disconnect', '/users/me', '/files'];
-  if (!paths.includes(request.path)) {
-    next();
-  } else if (!request.headers['x-token']) {
-    response.status(401).json({ error: 'Unauthorized' }).end();
-  } else {
-    next();
-  }
-});
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+  // UsersController
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
 
-router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
-router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
-module.exports = router;
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
+
+  // AuthController
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  // FilesController
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
+
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+};
+
+module.exports = cRouting;
