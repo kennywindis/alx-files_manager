e814a8cde1551609abd5c994aa797c4c40d0d50f
kennywindis <kennyphilip100@gmail.com>
2023-06-28 04:49:26 -0700

    fm

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,16 +1,32 @@
-import { v4 as uuidv4 } from 'uuid';
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+import { Buffer } from 'buffer';
+import { v4 } from 'uuid';
+import redisClient from '../utils/redis';
+import UtilController from './UtilController';
+import dbClient from '../utils/db';
 
-const { createHash } = require('crypto');
+export default class AuthController {
+  static async getConnect(request, response) {
+    try {
+      const encodeAuthPair = request.headers.authorization.split(' ')[1];
+      const decodeAuthPair = Buffer.from(encodeAuthPair, 'base64').toString().split(':');
+      const _email = decodeAuthPair[0];
+      const pwd = UtilController.SHA1(decodeAuthPair[1]);
+      const user = await dbClient.filterUser({ email: _email });
+      if (user.password !== pwd) {
+        response.status(401).json({ error: 'Unauthorized' }).end();
+      } else {
+        const _token = v4();
+        await redisClient.set(`auth_${_token}`, user._id.toString(), 86400);
+        response.status(200).json({ token: _token }).end();
+      }
+    } catch (e) {
+      response.status(401).json({ error: 'Unauthorized' }).end();
+    }
+  }
 
-class AuthController {
-  static getConnect(request, response) {
-    (async () => {
-      // use authorization header to get the token, remove 'base', and split by ' '
-      const unSplit = Buffer.from(request.headers.authorization.split(' ')[1], 'base64').toString('ascii');
-      const [email, password] = unSplit.split(':');
-      // If not found, return an error Unauthorized with a status code 401
-      if (!email || !password) return response.status(401).send({ error: 'Unauthorized' });
-      // utilize createHash to hash the password
-
+  static async getDisconnect(request, response) {
+    const { token } = request;
+    await redisClient.del(token);
+    response.status(204).end();
+  }
+}
