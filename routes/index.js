0b10fdaca2c202cdf64c1392d29032fc98e7a905
kennywindis <kennyphilip100@gmail.com>
2023-06-28 04:38:32 -0700

    fm

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,20 @@
-// File containing all endpoints of API
-const express = require('express');
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
-// import controllers
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
+export default function Routes(app) {
+  const router = express.Router();
+  app.use('/', router);
 
-// router setup
-const router = (app) => {
-  const paths = express.Router();
-  app.use(express.json());
-  app.use('/', paths);
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
 
-  // GET '/status'
-  paths.get('/status', AppController.getStatus);
-  // GET '/stats'
-  paths.get('/stats', AppController.getStats);
-  // POST '/users'
-  paths.post('/users', UsersController.postNew);
-  // GET '/connect'
-  paths.get('/connect', AuthController.getConnect);
-  // GET '/disconnect'
-  paths.get('/disconnect', AuthController.getDisconnect);
-  // GET '/users/me'
-  paths.get('/users/me', UsersController.getMe);
-  // POST '/files'
-  paths.post('/files', FilesController.postUpload);
-  // GET '/files/:id'
-  paths.get('/files/:id', FilesController.getShow);
-  // GET '/files'
-  paths.get('/files', FilesController.getIndex);
-  // PUT '/files/:id/publish'
-  paths.put('/files/:id/publish', FilesController.putPublish);
-  // PUT '/files/:id/publish
-  paths.put('/files/:id/unpublish', FilesController.putUnpublish);
-  // GET '/files/:id/data'
-  paths.get('/files/:id/data', FilesController.getFile);
-};
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-module.exports = router;
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+}
